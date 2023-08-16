a5959604527fed772101d2c126cd618be6257544
Sasha-Polgar <sasha.polgar@hotmail.com>
2023-08-16 09:36:33 +0200

    Etape 2

--- index.js
+++ index.js
@@ -8,18 +8,17 @@ const express = require("express");
 * Configuration d'express
 *==============================*/
 const app = express();
-
 const port = 3000;
-
 app.set("view engine", "ejs");
-
 app.set("views", "views");
-
+app.use(express.static("public"));
 
 /*============================
 * Définitions des routes
 *==============================*/
-
+app.get("/",(req, res) => {
+    res.render("index",)
+})
 
 /*============================
 * Lancement du serveur
