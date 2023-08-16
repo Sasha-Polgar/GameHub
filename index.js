c601707a7ed8b0218d34f933bacfff4ada79bb7e
Sasha-Polgar <sasha.polgar@hotmail.com>
2023-08-16 12:08:51 +0200

    Etape 3 et 4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * Import des modules
 *==============================*/
 const express = require("express");
-
+const games = require("./games.json");
 
 /*============================
 * Configuration d'express
@@ -12,13 +12,31 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static("public"));
+app.locals.games = games;
 
 /*============================
 * Définitions des routes
 *==============================*/
 app.get("/",(req, res) => {
-    res.render("index",)
+    res.render("index")
 })
+
+app.get("/game/fourchette", (req, res) => {
+    res.render("fourchette")
+})
+
+app.get("/game/diceRoller", (req, res) => {
+    res.render("diceRoller", {css:true})
+});
+
+// app.get("/game/:nomDuJeu", (req, res) => {
+//     let gameUrl = req.params.nomDuJeu;
+//     let findGame = games.find((game) => game.name === nomDuJeu);
+//     if(!findGame) {
+//         res.status(404);
+//     }
+//     res.render({game:findGame})
+// })
 
 /*============================
 * Lancement du serveur
